6f502ae2a74e0e62a169e507a0c94ff459c3b8f6
markipooos <markhumphries88@gmail.com>
2023-01-18 16:31:49 +0000

    caesar cypher completed

--- src/caesar_cipher/caesar_cipher.py
+++ src/caesar_cipher/caesar_cipher.py
@@ -1,3 +1,27 @@
 
 def caesar(string, number):
+    returnstring=""
+
+    for letter in string:
+        upper=0
+        if 61<=ord(letter.lower())<=122:
+            if letter.isupper()==True:
+                upper=1
+            newletter=(ord(letter.lower())+number)
+
+            if newletter>122:
+                newletter=newletter%122+96
+
+            if newletter<97:
+                newletter=122-(96-newletter%97)
+            
+            if upper==1:
+                returnstring+=chr(newletter).upper()
+            else:
+                returnstring+=chr(newletter)
+        else:
+            returnstring+=letter
+    return returnstring
     pass
+
+
---
+++ test/test_caesar_cipher.py
@@ -0,0 +1,36 @@
+from src.caesar_cipher.caesar_cipher import caesar
+
+def test_shifts_single_letter_1():
+    expected="y"
+    result=caesar("x",1)
+    assert expected==result
+
+    expected="w"
+    result=caesar("x",-1)
+    assert expected==result
+
+def test_shifts_with_a_loop_around():
+    expected="a"
+    result=caesar("z",1)
+    assert expected==result
+
+    expected="z"
+    result=caesar("a",-1)
+    assert expected==result
+
+
+
+def test_shifts_word():
+    expected="jgnnq"
+    result=caesar("hello",2)
+    assert expected==result
+
+def test_ignores_spaces():
+    expected="ebiil tloia!"
+    result=caesar("hello world!",-3)
+    assert expected==result
+
+def test_word_wrap_around():
+    expected="vwxyz"
+    result=caesar("abcde",-5)
+    assert expected==result
