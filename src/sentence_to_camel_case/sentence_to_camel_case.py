80640cb9bd61620915db344771a246d7d0bdcfde
markipooos <markhumphries88@gmail.com>
2023-01-18 11:12:38 +0000

    Completed camel case and reverse camel case

--- src/sentence_to_camel_case/sentence_to_camel_case.py
+++ src/sentence_to_camel_case/sentence_to_camel_case.py
@@ -1,2 +1,31 @@
-def sentence_to_camel_case(arg1, arg2):
+def sentence_to_camel_case(arg1, *arg2,reverse=False):
+    finalStr=""
+    if len(arg2)>0:
+        list=arg1.split(" ")
+        if arg2[0]==True:
+            for word in list:
+                finalStr+=word[0].upper()+word[1:]
+        elif arg2[0]==False:
+            finalStr+=list[0][0].lower()+list[0][1:]
+
+            for word in list[1:]:
+                finalStr+=word[0].upper()+word[1:]
+    if reverse != False:
+       
+        finalStr+=arg1[0][0].upper()
+        
+        for letter in arg1[1:]:
+            if letter.isupper()==True:
+                finalStr+=" "+letter.lower()
+            else:
+                finalStr+=letter
+        finalStr+="."
+    
+            
+
+
+
+    return finalStr
+
+
     pass
--- test/test_sentence_to_camel_case.py
+++ test/test_sentence_to_camel_case.py
@@ -7,3 +7,18 @@ def test_returns_a_single_word_in_upper_if_true():
     result = sentence_to_camel_case('this', True)
     print(f'Result is: {result}')
     assert result == expected
+
+def test_returns_a_sentence_in_upper_if_true():
+    expected="ThisSentence"
+    result=sentence_to_camel_case("this sentence",True)
+    assert result==expected
+
+def test_returns_lower_camel_if_false():
+    expected="thisSentence"
+    result=sentence_to_camel_case("this sentence",False)
+    assert result==expected
+
+def test_returns_reversed_if_passed_third_arguement_called_as_reverse():
+    expected="This bigger strange sentence."
+    result=sentence_to_camel_case("thisBiggerStrangeSentence",reverse=True)
+    assert result==expected
