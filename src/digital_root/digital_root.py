54a4a9ee600ab48c72fcdd60826af822dcbd16b3
markipooos <markhumphries88@gmail.com>
2023-01-25 14:30:49 +0000

    Digital root completed

--- src/digital_root/digital_root.py
+++ src/digital_root/digital_root.py
@@ -8,7 +8,7 @@ def digital_root(num):
     while(num>1):
       currentpass+=num%10
       num=math.floor(num/10)
-      print(num)
+     
     currentpass+=num
     return digital_root(currentpass)
   pass
